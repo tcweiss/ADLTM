98c2b1484cfc7b2d92c5d243954915294d83385b
Marc Robin Gruener <gmarcrobin@outlook.de>
2020-10-28 20:41:28 +0100

    Test if data of sensors are normally distributed

---
+++ Marc/0_1_Normal_Dist_Test.py
@@ -0,0 +1,12 @@
+from scipy import stats
+import pandas as pd
+
+df_all = pd.read_csv('ALL_DATA_IBM_2020-06-05-00-00-00_to_2020-10-18-16-25-33.csv', parse_dates=True)
+df_all = df_all.drop('Unnamed: 0', axis=1)
+df_all['TS'] = pd.to_datetime(df_all['TS'], yearfirst=True)
+df_all = df_all.set_index('TS', drop=True, verify_integrity=True)
+
+k, p = stats.normaltest(df_all)
+
+print(df_all)
+print('Max P-Value:', max(p))
